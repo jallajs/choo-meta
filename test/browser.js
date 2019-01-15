ccc0d729cf2b37a0099fd73aa377be2afd515d22
Carl Törnqvist <carl@tornqv.ist>
2019-01-15 10:28:32 +0100

    Fix test race condition

--- test/browser.js
+++ test/browser.js
@@ -52,13 +52,15 @@ test('prevent appending missing nodes', function (t) {
   app.mount('body')
 
   window.requestAnimationFrame(function () {
-    var title = document.querySelector('title')
-    t.equal(title.innerHTML, 'foo', 'title was updated')
+    window.requestAnimationFrame(function () {
+      var title = document.querySelector('title')
+      t.equal(title.innerHTML, 'foo', 'title was updated')
 
-    var ogTitle = document.querySelector(`meta[property="og:title"]`)
-    t.notOk(ogTitle, 'og:title tag was not added')
+      var ogTitle = document.querySelector(`meta[property="og:title"]`)
+      t.notOk(ogTitle, 'og:title tag was not added')
 
-    var description = document.querySelector(`meta[name="description"]`)
-    t.notOk(description, 'meta description tag was not added')
+      var description = document.querySelector(`meta[name="description"]`)
+      t.notOk(description, 'meta description tag was not added')
+    })
   })
 })
