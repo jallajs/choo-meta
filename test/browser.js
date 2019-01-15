9cef798c86dc3931716bc5fe7b84297dcd22f159
Carl Törnqvist <carl@tornqv.ist>
2019-01-15 15:07:56 +0100

    Fix tests (for realz)

--- test/browser.js
+++ test/browser.js
@@ -3,11 +3,8 @@ var choo = require('choo')
 var html = require('choo/html')
 var meta = require('../')
 
-var HEAD_HTML = document.head.innerHTML
-
 test('append missing nodes by default', function (t) {
   t.plan(7)
-  document.head.innerHTML = HEAD_HTML
   var app = choo()
   app.use(meta())
   app.route('/*', main)
@@ -34,33 +31,5 @@ test('append missing nodes by default', function (t) {
     var ogTest = document.querySelector(`meta[property="og:test"]`)
     t.ok(ogTest, 'og:test tag added')
     t.equal(ogTest.getAttribute('content'), 'baz', 'og:test content match')
-  })
-})
-
-test('prevent appending missing nodes', function (t) {
-  t.plan(3)
-  document.head.innerHTML = HEAD_HTML
-  window.requestAnimationFrame(function () {
-    var app = choo()
-    app.use(meta({ append: false }))
-    app.route('/*', main)
-
-    function main (state, emit) {
-      emit('meta', { title: 'foo', description: 'bar' })
-      return html`<body></body>`
-    }
-
-    app.mount('body')
-
-    window.requestAnimationFrame(function () {
-      var title = document.querySelector('title')
-      t.equal(title.innerHTML, 'foo', 'title was updated')
-
-      var ogTitle = document.querySelector(`meta[property="og:title"]`)
-      t.notOk(ogTitle, 'og:title tag was not added')
-
-      var description = document.querySelector(`meta[name="description"]`)
-      t.notOk(description, 'meta description tag was not added')
-    })
   })
 })
